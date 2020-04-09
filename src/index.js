6833b647d96bd67c6f9108bb0fe6e7bcc2555137
rabah babaci <4rabah@gmail.com>
2020-04-08 21:12:32 -0700

    get the App working correctly

--- src/index.js
+++ src/index.js
@@ -1,21 +1,84 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+
 import './styles.scss';
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+
+const incrementValue = () => ({
+  type: INCREMENT,
+});
+const decrementValue = () => ({
+  type: DECREMENT,
+});
+const resetValue = () => ({
+  type: RESET,
+});
+
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    case RESET:
+      return { count: 0 };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
 
 class Counter extends Component {
   render() {
+    const { count, increment, decrement, reset } = this.props;
+
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+          <button onClick={reset}>Reset</button>
         </section>
       </main>
     );
   }
 }
 
-render(<Counter />, document.getElementById('root'));
+const mapStateToProps = (state) => state;
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment() {
+      dispatch(incrementValue());
+    },
+    decrement() {
+      dispatch(decrementValue());
+    },
+    reset() {
+      dispatch(resetValue());
+    },
+  };
+};
+
+// const mapDispatchToProps = {
+//   increment: incrementValue,
+//   decrement: decrementValue,
+//   reset: resetValue,
+// };
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+render(
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>,
+  document.getElementById('root'),
+);
